c5365c8a0619c78d2f0d09c18f22c6f6b4a80afa
Hamad-Pervaiz <technicalprojectmanager@blackinc.ae>
2025-03-08 09:23:04 +0500

    Second extract

--- js/grooveclickmagic.js
+++
@@ -1,7 +0,0 @@
-    window.clickmagick_cmc = {
-        uid: '180037',
-        hid: '2915347188',
-        cmc_project: 'FB - Breathe',
-        vid_info: 'on',
-        log_action: '.link-button:Quiz+CTA+Button',
-    }
